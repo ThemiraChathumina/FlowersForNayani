0d1b66d1979adedeba8945b917469c9d5531873c
ThemiraChathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2024-06-05 11:43:59 +0530

    Reduce stars

--- src/components/night/night.tsx
+++ src/components/night/night.tsx
@@ -1,11 +1,11 @@
 import "./night.css";
 
 export default function Night() {
-  const stars = Array.from({ length: 500 }, (_, index) => {
+  const stars = Array.from({ length: 300 }, (_, index) => {
     const style = {
       top: `${Math.random() * 100}vh`,
       left: `${Math.random() * 100}vw`,
-      animationDelay: `${Math.random() * 5}s`,
+      animationDelay: `${Math.random() * 4}s`,
     };
     return <div key={index} className="star" style={style}></div>;
   });
